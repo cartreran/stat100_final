aa4f8d8afb4c1c636186f11cdac57cf20c0fdac8
Gage LeMaster <lemasng0@sewanee.edu>
2024-10-29 16:03:55 -0500

    Test

--- recruitment.r
+++ recruitment.r
@@ -2,7 +2,7 @@ library(tidyverse)
 library(readr)
 
 transfer <- read.csv("./data/Recruitment/transfer.csv")
-
+#TEST
 ggplot(data = transfer, aes(x = `YEAR`, y = `TRATE`)) +
   geom_line() +
   geom_point()
