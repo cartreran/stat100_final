40052fb79c5208ddac5769f11c5529ee63af58b4
carterdacat <56525188+carterdacat@users.noreply.github.com>
2024-10-31 15:48:26 -0500

    Working on Reading Analysis

--- recruitment.r
+++ recruitment.r
@@ -1,8 +1,12 @@
 library(tidyverse)
 library(readr)
 
-transfer <- read.csv("./data/Recruitment/transfer.csv")
+transfer <- read.csv("./data/transfer.csv")
 
-ggplot(data = transfer, aes(x = `YEAR`, y = `TRATE`)) +
-  geom_line() +
-  geom_point()
+transfer$YEAR <- as.factor(transfer$YEAR)
+
+## transfers by year
+ggplot() +
+    geom_col(data = transfer, aes(x = `YEAR`, y = `TEAM_COUNT`), show.legend = TRUE, just = 1) +
+    theme_minimal() +
+    labs(x = "Year", y = "Transfer Count", title = "Conference Transfers by Year")
