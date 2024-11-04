bece6f14f64c1c9fe13ea7cfc840e8aed65277f8
carterdacat <56525188+carterdacat@users.noreply.github.com>
2024-11-03 21:01:38 -0600

    Graphing

--- recruitment.r
+++ recruitment.r
@@ -10,3 +10,4 @@ ggplot() +
     geom_col(data = transfer, aes(x = `YEAR`, y = `TEAM_COUNT`), show.legend = TRUE, just = 1) +
     theme_minimal() +
     labs(x = "Year", y = "Transfer Count", title = "Conference Transfers by Year")
+
