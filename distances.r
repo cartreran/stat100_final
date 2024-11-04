e9becc9045ecdd1410931f3c6c994513c7fd20eb
carterdacat <56525188+carterdacat@users.noreply.github.com>
2024-11-03 20:07:56 -0600

    Documentation to distances

--- distances.r
+++ distances.r
@@ -5,21 +5,30 @@ library(geosphere)
 
 # Read in the official FBS list
 fbs_list <- read_csv("./data/FBS_list.csv") %>%
+
+  # Rename the conference column to remove the new line
   rename(conference = `Current\r\nConference`) %>%
+
+  # Select relevant columns
   select(School, conference) %>%
   mutate(
+
     # Correct the name of the school
     School = recode(School,
     "Louisiana–Monroe" = "Louisiana-Monroe")
+
   )
 
 # Read in the stadiums data
 # Source: https://github.com/gboeing/data-visualization/tree/main/ncaa-football-stadiums/data
 stadiums <- read_csv("./data/stadiums-geocoded.csv") %>%
+
   # Select relevant columns
   select(team, capacity, div, latitude, longitude) %>%
+
   # Filter for FBS teams and exclude Idaho
   filter(div == "fbs" & team != "Idaho") %>%
+
   # Add rows for new FBS teams
   add_row(team = "Coastal Carolina", capacity = 21000, div = "fbs", latitude = 33.7930, longitude = -79.0177) %>%
   add_row(team = "Jacksonville State", capacity = 24000, div = "fbs", latitude = 33.8203, longitude = -85.7664) %>%
@@ -28,6 +37,7 @@ stadiums <- read_csv("./data/stadiums-geocoded.csv") %>%
   add_row(team = "Liberty", capacity = 25000, div = "fbs", latitude = 37.3523, longitude = -79.1716) %>%
   add_row(team = "Sam Houston", capacity = 14000, div = "fbs", latitude = 30.7083, longitude = -95.5383) %>%
   add_row(team = "UAB", capacity = 47100, div = "fbs", latitude = 33.4971, longitude = -86.8121) %>%
+
   # Correct team names
   mutate(
      team = recode(team,
@@ -42,6 +52,7 @@ stadiums <- read_csv("./data/stadiums-geocoded.csv") %>%
     )
   )
         
+# Combine the stadiums and FBS list
 teams <- stadiums %>%
   left_join(fbs_list, by = c("team" = "School"))
 
@@ -50,13 +61,17 @@ teams <- stadiums %>%
 # Load the schedule dataset
 # https://www.sports-reference.com/cfb/years/2024-schedule.html
 schedule <- read_csv("./data/2024_schedule.csv") %>% 
+
+  # Rename columns and remove weekly rankings
   mutate(
     home = gsub("\\([0-9]{1,2}\\)\\s*", "", winner), 
     away = gsub("\\([0-9]{1,2}\\)\\s*", "", loser),
     home_pts = win_pts,
     away_pts = lose_pts
   ) %>%
-    mutate(
+
+  # Correct team names
+  mutate(
     home = recode(home,
       "Connecticut" = "UConn",
       "North Carolina State" = "NC State",
@@ -96,15 +111,18 @@ schedule <- read_csv("./data/2024_schedule.csv") %>%
       "Alabama-Birmingham" = "UAB"
     )
   ) %>%
-  # Adjust for neutral site games
+
+  # Resort away and home teams
   mutate(
     temp_home = ifelse(grepl("@", away_neutral), away, home),
     temp_away = ifelse(grepl("@", away_neutral), home, away),
     temp_home_pts = ifelse(grepl("@", away_neutral), away_pts, home_pts),
     temp_away_pts = ifelse(grepl("@", away_neutral), home_pts, away_pts)
   ) %>%
+
   # Filter for games involving FBS teams
   filter(temp_away %in% stadiums$team) %>%
+  
   # Remove unnecessary columns and rename temporary columns
   select(-home, -away, -home_pts, -away_pts, -winner, -loser, -win_pts, -lose_pts) %>%
   rename(
@@ -119,24 +137,43 @@ schedule <- read_csv("./data/2024_schedule.csv") %>%
 # calculate disantance for each and add together and 
 # assign to team
 
+# Add a column for total mileage
 teams$total_mileage <- 0
 
-conf_mileage <- teams %>%
-  group_by(conference) %>%
-  summarise(total_mileage = sum(total_mileage))
-
+# Loop through each team and calculate the total mileage
 for(i in 1:nrow(teams)){
+
+  # Get the team and coordinates
   team <- teams$team[i]
   team_lat <- round(teams$latitude[i], digits = 4)
   team_long <- round(teams$longitude[i], digits = 4)
+
+  # Get the away games
   away_games <- schedule[schedule$away == team, "home"]
+
+  # reset mileage to 0
   total_mileage <- 0
+
+  # Loop through each away game and calculate the distance
   for(j in 1:nrow(away_games)){
+
+    # Get the coordinates of the home team
     home_team <- away_games$home[j]
     home_lat <- round(teams[teams$team == home_team, "latitude"]$latitude[1], digits = 4)
     home_long <- round(teams[teams$team == home_team, "longitude"]$longitude[1], digits = 4)
+
+    # Calculate the distance in miles
     distance <- distHaversine(matrix(c(team_long, team_lat), ncol = 2), matrix(c(home_long, home_lat), ncol = 2)) / 1609.34
+
+    # Add the distance to the total mileage
     total_mileage <- total_mileage + distance
   }
+
+  # Assign the total mileage to the team and round
   teams$total_mileage[i] <- round(total_mileage)
 }
+
+# Calculate conference mileage
+conf_mileage <- teams %>%
+  group_by(conference) %>%
+  summarise(total_mileage = sum(total_mileage))
